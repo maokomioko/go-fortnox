78dc4a9650be77176ba5433d6ea0fcb54a2f30f0
Oleg Karpenko <karpenko.oleg.tr@gmail.com>
2022-08-15 19:10:31 +0300

    add delete_customer

--- delete_customer.go
+++ delete_customer.go
@@ -1,1 +1,121 @@
 package fortnox
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/omniboost/go-fortnox/utils"
+)
+
+func (c *Client) NewDeleteCustomerRequest() DeleteCustomerRequest {
+	r := DeleteCustomerRequest{
+		client:  c,
+		method:  http.MethodDelete,
+		headers: http.Header{},
+	}
+
+	r.queryParams = r.NewDeleteCustomerQueryParams()
+	r.pathParams = r.NewDeleteCustomerPathParams()
+	r.requestBody = r.NewDeleteCustomerRequestBody()
+	return r
+}
+
+type DeleteCustomerRequest struct {
+	client      *Client
+	queryParams *DeleteCustomerQueryParams
+	pathParams  *DeleteCustomerPathParams
+	method      string
+	headers     http.Header
+	requestBody DeleteCustomerRequestBody
+}
+
+func (r DeleteCustomerRequest) NewDeleteCustomerQueryParams() *DeleteCustomerQueryParams {
+	return &DeleteCustomerQueryParams{}
+}
+
+type DeleteCustomerQueryParams struct{}
+
+func (p DeleteCustomerQueryParams) ToURLValues() (url.Values, error) {
+	encoder := utils.NewSchemaEncoder()
+	params := url.Values{}
+
+	err := encoder.Encode(p, params)
+	if err != nil {
+		return params, err
+	}
+
+	return params, nil
+}
+
+func (r *DeleteCustomerRequest) QueryParams() *DeleteCustomerQueryParams {
+	return r.queryParams
+}
+
+func (r DeleteCustomerRequest) NewDeleteCustomerPathParams() *DeleteCustomerPathParams {
+	return &DeleteCustomerPathParams{}
+}
+
+type DeleteCustomerPathParams struct {
+	CustomerNumber string
+}
+
+func (p *DeleteCustomerPathParams) Params() map[string]string {
+	return map[string]string{
+		"CustomerNumber": p.CustomerNumber,
+	}
+}
+
+func (r *DeleteCustomerRequest) PathParams() *DeleteCustomerPathParams {
+	return r.pathParams
+}
+
+func (r *DeleteCustomerRequest) SetMethod(method string) {
+	r.method = method
+}
+
+func (r *DeleteCustomerRequest) Method() string {
+	return r.method
+}
+
+func (r DeleteCustomerRequest) NewDeleteCustomerRequestBody() DeleteCustomerRequestBody {
+	return DeleteCustomerRequestBody{}
+}
+
+type DeleteCustomerRequestBody struct{}
+
+func (r *DeleteCustomerRequest) RequestBody() *DeleteCustomerRequestBody {
+	return &r.requestBody
+}
+
+func (r *DeleteCustomerRequest) SetRequestBody(body DeleteCustomerRequestBody) {
+	r.requestBody = body
+}
+
+func (r *DeleteCustomerRequest) NewResponseBody() *DeleteCustomerResponseBody {
+	return &DeleteCustomerResponseBody{}
+}
+
+type DeleteCustomerResponseBody struct {
+}
+
+func (r *DeleteCustomerRequest) URL() url.URL {
+	return r.client.GetEndpointURL("/customers/{{.CustomerNumber}}", r.PathParams())
+}
+
+func (r *DeleteCustomerRequest) Do() (DeleteCustomerResponseBody, error) {
+	// Create http request
+	req, err := r.client.NewRequest(nil, r.Method(), r.URL(), nil)
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	// Process query parameters
+	err = utils.AddQueryParamsToRequest(r.QueryParams(), req, false)
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	responseBody := r.NewResponseBody()
+	_, err = r.client.Do(req, responseBody)
+	return *responseBody, err
+}
--- delete_customer_test.go
+++ delete_customer_test.go
@@ -1,1 +1,19 @@
-package fortnox
+package fortnox_test
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+)
+
+func TestDeleteCustomer(t *testing.T) {
+	req := client.NewDeleteCustomerRequest()
+	req.NewDeleteCustomerPathParams().CustomerNumber = "1"
+	resp, err := req.Do()
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := json.MarshalIndent(resp, "", "  ")
+	log.Println(string(b))
+}
